b1047294ef831e1388d47af222e24dbbbdc628f4
phamthevinhtv <phamthevinhtv@gmail.com>
2025-04-17 07:50:34 +0700

    Add script

---
+++ script.js
@@ -0,0 +1,6 @@
+const btnAction = document.getElementById("actionButton");
+const info = document.getElementById("info");
+
+btnAction.addEventListener('click', function() {
+    info.style.display = "flex";
+});
